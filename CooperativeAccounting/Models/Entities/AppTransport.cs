6aa23c84c92707876c746c3e75f1bd0d3bd59536
Haruna Salim <salxsaa@gmail.com>
2018-05-05 22:22:28 +0100

    Data Analysis

---
+++ CooperativeAccounting/Models/Entities/AppTransport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CooperativeAccounting.Models.Entities
+{
+    public class AppTransport
+    {
+        public List<Transaction> Transactions { get; set; }
+        public List<AppUser> AppUsers { get; set; }
+        public decimal CurrentBalance { get; set; }
+    }
+}
